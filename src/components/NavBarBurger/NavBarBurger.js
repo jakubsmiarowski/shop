28bf5963635d4eaa2bec135e2d0c8a45cb823a99
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-11-13 20:34:17 +0100

    mobile done

--- src/components/NavBarBurger/NavBarBurger.js
+++ src/components/NavBarBurger/NavBarBurger.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { slide as Menu } from "react-burger-menu";
+import { push as Menu } from "react-burger-menu";
 import './NavBarBurger.css';
 
 function NavBarBurger(props) {
